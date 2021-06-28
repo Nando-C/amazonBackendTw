634d379d8098d1803d1b50ecf372e1bf432ed388
Mike Oxhuge <mikelitoris34@icloud.com>
2021-06-28 11:02:54 +0200

    commit on main -m GET all reviews

--- src/services/reviews/index.js
+++ src/services/reviews/index.js
@@ -1,9 +1,20 @@
 import express from "express"; // 3rd party package
-import fs from "fs"; // core package
-import { fileURLToPath } from "url"; // core package
-import { dirname, join } from "path"; // core package
 import uniqid from "uniqid"; // 3rd party package
+import {
+  getReviews,
+  writeReviews,
+  writeReviewsPicture,
+} from "../../lib/fs-tools.js";
 
 const reviewsRouter = express.Router();
+
+reviewsRouter.get("/", async (req, res, next) => {
+  try {
+    const reviews = await getReviews();
+    res.send(reviews);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default reviewsRouter;
